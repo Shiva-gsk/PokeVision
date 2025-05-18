58325793663199500f3ff6f107dd8cd5d945661d
Shiva <shivagulapala2005@gmail.com>
2025-05-18 23:39:02 +0530

    Solved Deployment Issues ig?

--- lib/db.ts
+++ lib/db.ts
@@ -1,5 +1,8 @@
 
-import { PrismaClient } from "./generated/prisma";
+// import { PrismaClient } from "./generated/prisma";
+
+import { PrismaClient } from "@prisma/client";
+
 
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
